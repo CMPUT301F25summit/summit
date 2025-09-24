4b9c3d26651d06d9a64f2a128ed2b7a56824f7b1
rabewarmor <rabewar@ualberta.ca>
2025-09-24 17:54:38 -0600

    Shape Color variable

--- app/src/main/java/com/example/summit/Shape.java
+++ app/src/main/java/com/example/summit/Shape.java
@@ -3,6 +3,7 @@ package com.example.summit;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "Blue"; 
 
     public Shape (int x, int y) {
         this.x = x;
