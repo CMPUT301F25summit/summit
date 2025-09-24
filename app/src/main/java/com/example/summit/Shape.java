6391cc27a4efbbb2fa91bc3d738668af125a66aa
aniketh1409 <anikeths@ualberta.ca>
2025-09-24 14:34:56 -0600

    Added Shape class with attributes

---
+++ app/src/main/java/com/example/summit/Shape.java
@@ -0,0 +1,17 @@
+package com.example.summit;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    public Shape (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {return x;}
+    public int getY() {return y;}
+
+    public void setX(int x) {this.x = x;}
+    public void setY(int y) {this.y = y;}
+}
